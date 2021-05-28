904ccb55326d90a2d63b5a283737471d50877e54
AlexisAlegria <alexisalegriav@gmail.com>
2021-05-28 02:13:46 +0000

    ejercicio accionado

--- src/js/index.js
+++ src/js/index.js
@@ -32,15 +32,19 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name}</h1>
-          <h2>${variables.role}</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <img src="${variables.avatarURL} " class="photo" />
+          <h1>${variables.name || "Empty name"}</h1>
+          <h2>${variables.role || "Empty Role"}</h2>
+          <h3>${variables.city || ""}, ${variables.country || ""}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            ${variables.twitter &&
+              `<li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>`}
+            ${variables.github &&
+              `<li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>`}
+            ${variables.linkedin &&
+              `<li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>`}
+            ${variables.instagram &&
+              `<li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>`}
           </ul>
         </div>
     `;
@@ -66,9 +70,9 @@ window.onload = function() {
     instagram: null,
     name: null,
     lastname: null,
-    role: null,
-    country: null,
-    city: null
+    role: "",
+    country: "",
+    city: ""
   };
   render(window.variables); //render the card for the first time
 
